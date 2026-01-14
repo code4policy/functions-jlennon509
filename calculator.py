0e5e28b4ff66a046e7bee0687168afd937e479a9
jlennon509 <jlennon@g.harvard.edu>
2026-01-14 14:55:40 -0500

    added calculator

--- calculator.py
+++ calculator.py
@@ -1,16 +1,16 @@
-def multiply(a,b):
-    return a * b
+def multiply(4,6):
+    return 4 * 6
 
-def add(a,b):
-	return a+b
+def add(7,7):
+	return 7+7
 
-def subtract(a,b):
-	return a-b
+def subtract(4,5):
+	return 4-5
 
-def divide(a,b):
-	return a/b
+def divide(2,.5):
+	return 2/.5
 
 
-print("I'm going use the calculator functions to multiply 5 and 6")
+print("This is 5 multiplied by 6:")
 x = multiply(5,6)
 print(x)
