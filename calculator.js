685c079ba8cbefdfa3bfa6bfcf707532bd9caf20
jlennon509 <jlennon@g.harvard.edu>
2026-01-14 15:50:36 -0500

    removed var so calculator will work

--- calculator.js
+++ calculator.js
@@ -13,3 +13,7 @@ function    add(a,b) {
 function divide(a,b) {
     return a / b
 }
+console.log("I'm going use the calculator functions to multiply 12 and 8")
+x = multiply(12,8)
+console.log(x)
+
